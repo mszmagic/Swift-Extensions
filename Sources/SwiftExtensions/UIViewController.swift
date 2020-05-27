e306c4e9c6a078a77c167798e0c53fe8e541cd61
Shunzhe Ma <me@msz.cool>
2020-05-26 21:55:40 -0400

    Adding functions to get the UISceneDelegate

---
+++ Sources/SwiftExtensions/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shunzhe Ma on 5/26/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func getSceneDelegate() -> UISceneDelegate {
+        return self.view.window?.windowScene?.delegate
+    }
+    
+    func getUIApplicationDelegate() -> UIApplicationDelegate {
+        return UIApplication.shared.delegate
+    }
+    
+}
